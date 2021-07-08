76cc308a3c2c26be31f4c74c3ff73029e95b901c
renatog17 <renato.gomes@zup.com.br>
2021-07-08 14:50:45 -0300

    impedindo que um usuario altere um produto que não seja seu

--- src/main/java/br/com/renato/mercadolivre/controller/ProdutoController.java
+++ src/main/java/br/com/renato/mercadolivre/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.renato.mercadolivre.config.security.TokenService;
 import br.com.renato.mercadolivre.controller.form.ImagemForm;
@@ -54,12 +56,20 @@ public class ProdutoController {
 		caracteristicaRepository.saveAll(caracteristicas);
 		return ResponseEntity.ok().build();
 	}
-	
+
 	@PostMapping(value = "/{id}/imagens")
 	@Transactional
-	public ResponseEntity adicionaImagens(@PathVariable Long id ,@Valid ImagemForm imagemForm) {
+	public ResponseEntity adicionaImagens(@PathVariable Long id, @Valid ImagemForm imagemForm,
+			@RequestHeader("Authorization") String token) {
 		Set<String> links = uploaderFake.envia(imagemForm.getImagens());
+
+		Long idUsuario = tokenService.getIdUsuario(token.substring(7));
+		Usuario usuario = usuarioRepository.findById(idUsuario).get();
 		Produto produto = produtoRepository.getById(id);
+
+		if (usuario.getId() != produto.getUsuario().getId()) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+		}
 		produto.associaImagens(links);
 		produtoRepository.save(produto);
 		return ResponseEntity.ok().build();
